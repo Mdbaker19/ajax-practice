420e99a1200acbed150def5cc3a0dd4a7225bc52
Matthew <bakebill20@yahoo.com>
2020-11-17 08:58:14 -0600

    linked ajax requests

--- JS/jokes-facts.js
+++ JS/jokes-facts.js
@@ -1,3 +1,16 @@
 $(document).ready(function (){
 
+    const chuck = $.ajax("http://api.icndb.com/jokes/random");
+
+    chuck.done((data) => {
+        console.log(data);
+    });
+
+    const codeQuote = $.ajax("http://quotes.stormconsultancy.co.uk/random.json");
+
+    codeQuote.done(data => {
+        console.log(data);
+    });
+
+
 });
