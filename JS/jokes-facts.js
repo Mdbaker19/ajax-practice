60bfeda820da6af6b2b740e47835fa62d624c3e5
Matthew <bakebill20@yahoo.com>
2020-11-18 14:53:05 -0600

    request call set up to display a quote or a joke in a rendered html element

--- JS/jokes-facts.js
+++ JS/jokes-facts.js
@@ -2,15 +2,58 @@ $(document).ready(function (){
 
     const chuck = $.ajax("http://api.icndb.com/jokes/random");
 
-    chuck.done((data) => {
-        console.log(data);
-    });
+    function logJoke(){
+        chuck.done((data) => {
+            $(".content").html(renderJoke(data));
+        });
+    }
 
     const codeQuote = $.ajax("http://quotes.stormconsultancy.co.uk/random.json");
 
-    codeQuote.done(data => {
-        console.log(data);
+    function logQuote(){
+        codeQuote.done(data => {
+            $(".content").html(renderQuote(data));
+        });
+    }
+
+    let count = 0;
+
+    $("#next").on("click", function (){
+        if(count % 2 === 0){
+            logJoke();
+        } else {
+            logQuote();
+        }
+        count++;
     });
+
+    $("#previous").on("click", function (){
+        if(count % 2 === 0){
+            logJoke();
+        } else {
+            logQuote();
+        }
+        count++;
+    });
+
+
+
+
+
+    function renderJoke(data){
+        let html = `<div class="card"><h1>Chuck Norris</h1>`;
+        html += `<p class="info">${data.value.joke}</p>`;
+        return html;
+    }
+    function renderQuote(data){
+        let html = `<div class="card"><h1>${data.author}</h1>`;
+        html += `<p class="info">${data.quote}</p>`;
+        return html;
+    }
+
+
+
+
 
 
 });
