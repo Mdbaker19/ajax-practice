b80f20de5db6f34ed5a8f4169a45c662c4f44714
Matthew <bakebill20@yahoo.com>
2020-11-29 18:11:05 -0600

    added count for filtered characters, added functionality for character status filtering with search filter

--- JS/rickNmorty.js
+++ JS/rickNmorty.js
@@ -1,47 +1,76 @@
 $(document).ready(function (){
    const url = "https://rickandmortyapi.com/api/";
+   const charSearchBar = $("#searchChars");
+   const charStatusBar = $("#charStatus");
+   const contentArea = $(".chars")[0];
+   let allCharacters = [];
+   let filteredCharacters = [];
+   let allCharCount = 0;
+   let filteredCharCount = 0;
 
    function getAllChars(counter){
       fetch(`${url}character/?page=${counter}`).then((r) => r.json()).then(data => {
-         // console.log(data.results);
+         console.log(data.results);
          for(let i = 0; i < data.results.length; i++) {
-            $(".chars")[0].insertAdjacentHTML("beforeend", render(data.results[i]));
+            allCharacters.push(data.results[i]);
+            allCharCount++;
+            contentArea.insertAdjacentHTML("beforeend", render(data.results[i]));
          }
+         $("#amountOfChars").html(allCharCount);
       }).catch(err => console.error(err));
    }
-
-   function searchChars(counter){
-      $(".chars")[0].innerHTML = "";
-      fetch(`${url}character/?page=${counter}`)
-          .then((r) => r.json())
-          .then((data) => {
-               data.results.forEach((obj) => {
-                  if($("#searchChars").val().length < 1){
-                     $(".chars")[0].innerHTML += render(obj);
-                  } else if(obj.name.toLowerCase().includes($("#searchChars").val().toLowerCase())){
-                     $(".chars")[0].innerHTML += render(obj);
-                  }
-               })
-      }).catch(err => console.error(err));
-   }
-
-   $("#searchChars").on("input", function (){
-      for(let i = 1; i < 35; i++){
-         searchChars(i);
-      }
-   });
 
    for (let i = 1; i < 35; i++) {
       getAllChars(i);
    }
 
+   function searchChars(){
+      contentArea.innerHTML = "";
+      filteredCharCount = 0;
+      if(filteredCharacters.length < 1){
+         filteredCharacters = allCharacters;
+      }
+      filteredCharacters.forEach((character) => {
+         if(charSearchBar.val().length < 1){
+            filteredCharCount++;
+            contentArea.innerHTML += render(character);
+         } else if(character.name.toLowerCase().includes(charSearchBar.val().toLowerCase())){
+            filteredCharCount++;
+            contentArea.innerHTML += render(character);
+         }
+      });
+      $("#currentFilteredAmount").html(filteredCharCount);
+   }
+
+   function filterLife(){
+      filteredCharacters = [];
+      contentArea.innerHTML = "";
+      filteredCharCount = 0;
+      allCharacters.forEach((character) => {
+         if(character.status === charStatusBar.val()){
+            filteredCharCount++;
+            filteredCharacters.push(character);
+            contentArea.insertAdjacentHTML("beforeend", render(character));
+         }
+         $("#currentFilteredAmount").html(filteredCharCount);
+      });
+   }
+
+   charSearchBar.on("input", function (){
+      searchChars();
+   });
+
+   charStatusBar.on("change", function (){
+      filterLife();
+   });
+
    function render(obj){
       return  `<div class="card">
-                    <h3># ${obj.id} -- ${obj.name}</h3>
+                    <h2>${obj.name}</h2>
                     <h4><strong>${obj.status}</strong></h4>
-                    <img src="https://rickandmortyapi.com/api/character/avatar/${obj.id}.jpeg" alt="character_Image" width="200px" height="200px">
+                    <img src="https://rickandmortyapi.com/api/character/avatar/${obj.id}.jpeg" class="charImage" alt="character_Image" width="200px" height="200px">
                     <h6>Location: ${obj.location.name}</h6>
               </div>`
    }
-   
+
 });
